ba37fa2bcfe41cf61773dee346be4f4b573ebb8f
ABHISHEK <ag199680.ag@gmail.com>
2025-03-30 22:12:17 +0530

    deploying on render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import LandingPage from './pages/landing';
+import Authentication from './pages/authentication';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Router>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/auth" element={<Authentication />} />
+          <Route path='/home's element={<HomeComponent />} />
+          <Route path='/:url' element={<VideoMeetComponent />} />
+        </Routes>
+      </AuthProvider>  
+     </Router>
   );
 }
 
